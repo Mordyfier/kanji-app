8da55d8124e2493f239abe0756cca1d3c4fb64f7
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-01-13 19:09:09 -0500

    cleaned up useEffect

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import KanjiCard from "./components/KanjiCard";
 export default function App() {
   
   const [kanji, setKanji] = useState("");
-  useEffect(() => { 
-    async function fetchData() {
-      const response = await fetch('https://kanjiapi.dev/v1/kanji/joyo');
-      const data = await response.json();
-      const cards = data.map(x => <KanjiCard key={x} kanji={x} />)
-      setKanji(cards);
-    }
-    fetchData();
-  }, [])
+
+  async function fetchData() {
+    const response = await fetch('https://kanjiapi.dev/v1/kanji/joyo');
+    const data = await response.json();
+    const cards = data.map(x => <KanjiCard key={x} kanji={x} />)
+    setKanji(cards);
+  }
+
+  useEffect(() => fetchData(), [])
 
   return (
     <div id="kanji-cards">
